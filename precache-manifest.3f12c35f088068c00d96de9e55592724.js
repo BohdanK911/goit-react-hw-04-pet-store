74e03997beacc4ac5733e324209f97c96ee62213
Bohdan Kucheriavyi <kucheriavyi.w32@gmail.com>
2019-07-25 16:05:43 +0300

    Updates

---
+++ precache-manifest.3f12c35f088068c00d96de9e55592724.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a639989b209cffdf672018833e8edc5",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "15debd27039adc512246",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.d6bb3c16.chunk.css"
+  },
+  {
+    "revision": "1a271bf44dc8642f0a49",
+    "url": "/goit-react-hw-04-pet-store/static/css/pet-page.b750ec84.chunk.css"
+  },
+  {
+    "revision": "770dc79cb5d3af7d453b",
+    "url": "/goit-react-hw-04-pet-store/static/css/pets-page.7f35cb3e.chunk.css"
+  },
+  {
+    "revision": "4c079b907e1c0ce2194e",
+    "url": "/goit-react-hw-04-pet-store/static/js/5.3da89e90.chunk.js"
+  },
+  {
+    "revision": "45b64e0803af844a2a50",
+    "url": "/goit-react-hw-04-pet-store/static/js/about-page.5b0e7147.chunk.js"
+  },
+  {
+    "revision": "15debd27039adc512246",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.2387c229.chunk.js"
+  },
+  {
+    "revision": "1a271bf44dc8642f0a49",
+    "url": "/goit-react-hw-04-pet-store/static/js/pet-page.57540df2.chunk.js"
+  },
+  {
+    "revision": "770dc79cb5d3af7d453b",
+    "url": "/goit-react-hw-04-pet-store/static/js/pets-page.475e5c60.chunk.js"
+  },
+  {
+    "revision": "ae3a831c0eb545bc39b4",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.91124b9a.js"
+  }
+]);
